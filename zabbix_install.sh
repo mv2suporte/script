a3f0f7da6e3dcec89c041a5dd7913d92c8fb0ccb
MV2 SOLUTIONS <118684183+mv2suporte@users.noreply.github.com>
2022-12-06 10:57:26 -0300

    Update zabbix_install.sh

--- zabbix_install.sh
+++ zabbix_install.sh
@@ -31,7 +31,7 @@ zabbix-apache-conf zabbix-sql-scripts zabbix-agent zabbix-get \
 zabbix-sender apache2 php php-cgi libapache2-mod-php \
 php-common php-pear php-mbstring php-mysql php-gd php-xml-util php-bcmath \
 php-net-socket nmap snmp snmp-mibs-downloader mariadb-server htop mtr -y
-echo
+sed -i 's/mibs :/# mibs :/g' /etc/snmp/snmp.conf
 echo
 echo "INICIANDO A CONFIGURAÇÃO DO MYSQL"
 sleep 3
