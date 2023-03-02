2d83ca51bd26b3d37a7310851218e02fae128391
MV2 SOLUTIONS <118684183+mv2suporte@users.noreply.github.com>
2023-03-02 20:00:09 -0300

    Update wppsendgrupo.sh

--- wppsendgrupo.sh
+++ wppsendgrupo.sh
@@ -1,9 +1,9 @@
 curl --request POST \
-  --url http://100.74.15.4:9001/sistema/sendTextGrupo \
+  --url http://api.mv2.solutions:9009/sistema/sendTextGrupo \
   --header 'Content-Type: application/json' \
   --data '{
         "AuthorizationToken": "rFqmeBuHlaq48IhMH7cuyMRhuDyU8W2lFClEx0",
-        "SessionName": "Mv2",
+        "SessionName": "rFqmeBuHlaq48IhMH7cuyMRhuDyU8W2lFClEx0",
         "groupId":"'$1'",
         "msg":"'"$2"'"
 }'
