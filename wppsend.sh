909fa1b6bab411279d9bd8673a8609637fcd433a
MV2 SOLUTIONS <118684183+mv2suporte@users.noreply.github.com>
2023-03-02 19:59:22 -0300

    Update wppsend.sh

--- wppsend.sh
+++ wppsend.sh
@@ -1,9 +1,9 @@
 curl --request POST \
-  --url http://100.74.15.4:9001/sistema/sendText \
+  --url http://api.mv2.solutions:9009/sistema/sendText \
   --header 'Content-Type: application/json' \
   --data '{
         "AuthorizationToken": "rFqmeBuHlaq48IhMH7cuyMRhuDyU8W2lFClEx0",
-        "SessionName": "Mv2",
+        "SessionName": "rFqmeBuHlaq48IhMH7cuyMRhuDyU8W2lFClEx0",
         "phonefull":"+55'$1'",
         "msg":"'"$2"'"
 }'
