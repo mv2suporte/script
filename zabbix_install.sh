ae0fb0ff95999fb915a722a24184e4df4e5df195
MV2 SOLUTIONS <118684183+mv2suporte@users.noreply.github.com>
2024-07-16 11:03:58 -0300

    Update zabbix_install.sh

--- zabbix_install.sh
+++ zabbix_install.sh
@@ -5,7 +5,7 @@ figlet -c "Script"
 figlet -c "de"
 figlet -c "Instalacao"
 figlet -c "ZABBIX"
-echo "Mv2 SOLUTIONS SUPORTE E CONSULTORIA EM TI http://mv2.solutions (24)99841-1506"
+echo "Mv2 SOLUTIONS SUPORTE E CONSULTORIA EM TI http://mv2.solutions (24)35110800"
 echo
 echo
 echo "ADICIONANDO REPOSITORIOS"
@@ -20,14 +20,13 @@ echo "INICIANDO O PROCESSO DE INSTALAÇÃO DO ZABBIX"
 echo
 sleep 3
 echo
-wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-4%2Bdebian11_all.deb
-dpkg -i zabbix-release_6.0-4+debian11_all.deb
+wget https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-release/zabbix-release_7.0-2+debian12_all.deb
+dpkg -i zabbix-release_7.0-2+debian12_all.deb
 apt update
 apt install locales -y
 locale-gen pt_BR.UTF-8
 update-locale LANG=pt_BR.UTF-8
-apt install zabbix-server-mysql zabbix-frontend-php \
-zabbix-apache-conf zabbix-sql-scripts zabbix-agent zabbix-get \
+apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent zabbix-get \
 zabbix-sender apache2 php php-cgi libapache2-mod-php \
 php-common php-pear php-mbstring php-mysql php-gd php-xml-util php-bcmath \
 php-net-socket nmap snmp snmp-mibs-downloader mariadb-server htop mtr curl -y
@@ -40,9 +39,12 @@ echo
 mysql -uroot -e "create database zabbix character set utf8mb4 collate utf8mb4_bin";
 mysql -uroot -e "create user zabbix@localhost identified by 'cisco'";
 mysql -uroot -e "grant all privileges on zabbix.* to zabbix@localhost";
+mysql -uroot -e "set global log_bin_trust_function_creators = 1";
 echo
 echo "CRIANDO O BANCO DE DADOS"
 zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -pcisco zabbix
+echo
+mysql -uroot -e "set global log_bin_trust_function_creators = 0";
 echo
 echo "CONFIGURANDO O PHP"
 sleep 3
@@ -57,12 +59,17 @@ echo
 echo "INSTALANDO O GRAFANA"
 sleep 3
 echo
-apt install adduser libfontconfig -y
-wget https://dl.grafana.com/enterprise/release/grafana-enterprise_9.2.2_amd64.deb
-dpkg -i grafana-enterprise_9.2.2_amd64.deb
-grafana-cli plugins install alexanderzobnin-zabbix-app
-grafana-cli plugins update alexanderzobnin-zabbix-app
-echo
+apt-get install -y apt-transport-https software-properties-common wget -y
+mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee -a /etc/apt/sources.list.d/grafana.list
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com beta main" | tee -a /etc/apt/sources.list.d/grafana.list
+apt-get update
+apt-get install grafana -y
+apt-get install grafana-enterprise -y
+#grafana-cli plugins list-remote
+#grafana-cli plugins install alexanderzobnin-zabbix-app
+systemctl restart grafana-server
 echo
 echo "ADICIONANDO ARQUIVOS NA INICIALIZAÇÃO"
 sleep 3
