bd35566790122a5a29ec860d1f7572a615dc978a
MV2 SOLUTIONS <118684183+mv2suporte@users.noreply.github.com>
2025-12-19 18:49:02 -0300

    Update zabbix_7.4_debian13_timescaledb.sh

--- zabbix_7.4_debian13_timescaledb.sh
+++ zabbix_7.4_debian13_timescaledb.sh
@@ -134,17 +134,17 @@ systemctl restart postgresql apache2 zabbix-server zabbix-agent
 
 echo "==== INSTALANDO O GRAFANA ===="
 sleep 3
-echo
-apt-get install -y apt-transport-https  wget -y
-mkdir -p /etc/apt/keyrings/
-wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee -a /etc/apt/sources.list.d/grafana.list
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com beta main" | tee -a /etc/apt/sources.list.d/grafana.list
-apt-get update
-apt-get install grafana -y
-apt-get install grafana-enterprise -y
-#grafana-cli plugins list-remote
-#grafana-cli plugins install alexanderzobnin-zabbix-app
+sudo apt-get install -y apt-transport-https wget
+sudo mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+# Updates the list of available packages
+sudo apt-get update
+# Installs the latest OSS release:
+sudo apt-get install grafana
+# Installs the latest Enterprise release:
+sudo apt-get install grafana-enterprise
 systemctl restart grafana-server
 systemctl enable grafana-server.service
 
