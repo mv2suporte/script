4e8a1b978fb42eaf059b89ca263cb4cc0d04e3cb
MV2 SOLUTIONS <118684183+mv2suporte@users.noreply.github.com>
2025-12-22 19:32:46 -0300

    Delete wppsendgrupo.sh

--- wppsendgrupo.sh
+++
@@ -1,9 +0,0 @@
-curl --request POST \
-  --url http://api.mv2.solutions:9009/group/sendTextGrupo \
-  --header 'AuthorizationToken: HDFTRE88776SBHGHFGYY2' \
-  --header 'Content-Type: application/json' \
-  --data '{
-        "SessionName": "HDFTRE88776SBHGHFGYY2",
-        "groupId":"'$1'",
-        "msg":"'"$2"'"
-}'
