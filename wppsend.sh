b242aea0ba67d412fb9aab5f367c14036c2a6d57
MV2 SOLUTIONS <118684183+mv2suporte@users.noreply.github.com>
2025-12-22 19:33:24 -0300

    Delete wppsend.sh

--- wppsend.sh
+++
@@ -1,9 +0,0 @@
-curl --request POST \
-  --url http://api.mv2.solutions:9009/message/sendText \
-  --header 'AuthorizationToken: HDFTRE88776SBHGHFGYY2' \
-  --header 'Content-Type: application/json' \
-  --data '{
-                "SessionName": "HDFTRE88776SBHGHFGYY2",
-                "phonefull": "+55'$1'",
-                "msg":"'"$2"'"
-}'
